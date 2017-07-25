a2df03f97f4638c246cbbfbe374d14a04a735c14
nbari <nbari@tequila.io>
2017-07-25 13:23:03 +0200

    new file:   Makefile 	new file:   github/handler.go 	new file:   github/hook.go 	new file:   github/hook_test.go 	new file:   healthCheck.go 	new file:   main.go

---
+++ github/handler.go
@@ -0,0 +1,42 @@
+package github
+
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+)
+
+type Commit struct {
+	Id      string `json:"id"`
+	Message string `json:"message"`
+	Url     string `json:"url"`
+}
+
+type Webhook struct {
+	Ref        string `json:"ref"`
+	Commits    []Commit
+	Repository struct {
+		Name           string `json:"name"`
+		Url            string `json:"url"`
+		Default_branch string `json:"default_branch"`
+	}
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	secret := []byte(os.Getenv("GITHUB_TOKEN"))
+	hook, err := Parse(secret, r)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	wh := new(Webhook)
+	decoder := json.NewDecoder(bytes.NewReader(hook.Payload))
+	if err := decoder.Decode(wh); err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	log.Printf("wh = %+v\n", wh)
+	log.Printf("msg = %s\n", wh.Commits[0].Message)
+}
